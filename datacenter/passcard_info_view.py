701997ecd3a4c1147f7ca4fda2c8fc251c14843e
Wolkodlakus <lesko68@list.ru>
2021-11-15 21:24:22 +0300

    Removed 'dict' in names

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -3,18 +3,19 @@ from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils.timezone import localtime, now
 
+
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.get(passcode=passcode)
     
-    this_passcard_visits = Visit.objects.filter(passcard = passcard)
+    this_passcard_visits = Visit.objects.filter(passcard=passcard)
     this_passcard_visits_context = []
     for this_passcard_visit in this_passcard_visits:
-        dict_visit = {
+        selected_characteristics = {
             'entered_at': localtime(this_passcard_visit.entered_at),
-            'duration' : this_passcard_visit.format_duration(this_passcard_visit.get_duration()),
-            'is_strange' : this_passcard_visit.is_long(),
+            'duration': this_passcard_visit.format_duration(this_passcard_visit.get_duration()),
+            'is_strange': this_passcard_visit.is_long(),
         }
-        this_passcard_visits_context.append(dict_visit)
+        this_passcard_visits_context.append(selected_characteristics)
     
     context = {
         'passcard': passcard,
